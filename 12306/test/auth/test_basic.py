434fba5c313cb48a2e593cbed057c198ccf0e773
Eason Li <lijinghua@odc.cmbchina.cn>
2020-01-01 20:25:41 +0800

    Add test for validate apptk

--- 12306/test/auth/test_basic.py
+++ 12306/test/auth/test_basic.py
@@ -22,3 +22,9 @@ class TestBasicAuth(TestCase):
             "Cookie": "uamtk=szOraZkX5L7qqSCiRwE1fElVZafQ_GImvB8wf-87Ip4tyl1l0"})
         result = TestBasicAuth.aut.get_apptk()
         self.assertFalse(result["get_apptk_successful"])
+
+    def test_validate_apptk(self):
+        TestBasicAuth.session.cookies = requests.utils.add_dict_to_cookiejar(TestBasicAuth.session.cookies, {
+            "Cookie": "uamtk=szOraZkX5L7qqSCiRwE1fElVZafQ_GImvB8wf-87Ip4tyl1l0"})
+        result = TestBasicAuth.aut.validate_apptk("MmngS7iLqTdAWwXSl-DBpxVO6DrrOyGg4tjoOU9jifA1pl1l0")
+        self.assertTrue(result["verify_successful"])
