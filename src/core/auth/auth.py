a15ca2ad9ac49fa85fca2e7a6b9e686fb0ec4ee0
EasonAndLily <15342349100@163.com>
2020-11-25 21:48:07 +0800

    Add abstract basic class for auth

--- src/core/auth/auth.py
+++ src/core/auth/auth.py
@@ -1,7 +1,12 @@
-class Auth(object):
+from abc import ABC, abstractmethod
+
+
+class Auth(ABC):
     def __init__(self, config):
+        super().__init__()
         self.username = config.USERNAME
         self.password = config.PASSWORD
 
+    @abstractmethod
     def login(self):
         pass
