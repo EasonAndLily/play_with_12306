ff5d35f71778e6e6003a8664a2e5cd72005e78bc
EasonAndLily <15342349100@163.com>
2020-11-25 21:54:05 +0800

    Refactor open login form page

--- src/core/auth/browser_auth.py
+++ src/core/auth/browser_auth.py
@@ -9,27 +9,18 @@ from selenium import webdriver
 class BrowserAuth(Auth):
     def __init__(self, config):
         super().__init__(config)
+        self.login_url = "https://kyfw.12306.cn/otn/resources/login.html"
+        self.driver = webdriver.Chrome()
 
     def login(self):
         self.open_login_form_page()
 
     def open_login_form_page(self):
-        driver = self.get_web_driver()
-        driver.get("https://kyfw.12306.cn/otn/resources/login.html")
-        login_form = driver.find_element_by_css_selector('.login-hd-account').find_element_by_tag_name('a')
-        actions = ActionChains(driver)
+        self.driver.get(self.login_url)
+        login_form = self.driver.find_element_by_css_selector('.login-hd-account').find_element_by_tag_name('a')
+        actions = ActionChains(self.driver)
         actions.move_to_element(login_form)
         actions.click(login_form)
         actions.perform()
         time.sleep(10)
 
-
-    @classmethod
-    def get_web_driver(cls):
-        options = webdriver.ChromeOptions()
-        options.add_argument(
-            '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-            '(KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36')
-        options.add_argument("--no-sandbox")
-        options.add_argument("--headless")
-        return webdriver.Chrome()
