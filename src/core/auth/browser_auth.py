f9f9b03052b64373948af169f82f596cd5a7d3bf
EasonAndLily <15342349100@163.com>
2020-11-28 23:53:35 +0800

    Implement slider verify

--- src/core/auth/browser_auth.py
+++ src/core/auth/browser_auth.py
@@ -13,17 +13,32 @@ class BrowserAuth(Auth):
     def __init__(self, config):
         super().__init__(config)
         self.login_url = "https://kyfw.12306.cn/otn/resources/login.html"
-        self.driver = webdriver.Chrome()
+        self.driver = BrowserAuth.generate_driver()
 
     def login(self):
+        self.driver.get(self.login_url)
+        self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
         self.open_login_form_page()
         self.fill_login_form()
         self.select_captcha_answers()
         self.submit_info()
-        time.sleep(100)
+        time.sleep(1)
+        self.slider_verify()
+        time.sleep(10)
+        self.driver.close()
+
+    @staticmethod
+    def generate_driver():
+        options = webdriver.ChromeOptions()
+        options.add_argument(
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36')
+        options.add_argument("--no-sandbox")
+        # options.add_argument("--headless") // if need run without window, use it
+        driver = webdriver.Chrome(options=options)
+        driver.maximize_window()
+        return driver
 
     def open_login_form_page(self):
-        self.driver.get(self.login_url)
         login_form = self.driver.find_element_by_css_selector('.login-hd-account').find_element_by_tag_name('a')
         actions = ActionChains(self.driver)
         actions.move_to_element(login_form)
@@ -56,3 +71,8 @@ class BrowserAuth(Auth):
         submit_btn = self.driver.find_element_by_id("J-login")
         actions = ActionChains(self.driver)
         actions.move_to_element(submit_btn).click().perform()
+
+    def slider_verify(self):
+        slider = self.driver.find_element_by_id("nc_1_n1z")
+        actions = ActionChains(self.driver)
+        actions.drag_and_drop_by_offset(slider, 300, 0).perform()
