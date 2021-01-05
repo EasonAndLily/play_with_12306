c5cc943f6b372abd89645f5e7271593180f46270
EasonAndLily <15342349100@163.com>
2021-01-05 23:00:30 +0800

    Refactor generate cookies

--- src/core/auth/browser_auth.py
+++ src/core/auth/browser_auth.py
@@ -17,32 +17,23 @@ class BrowserAuth(Auth):
 
     def login(self):
         self.driver.get(self.login_url)
-        self.cookies = self.driver.get_cookies()
-        self.generate_cookies()
         self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
         self.open_login_form_page()
+        time.sleep(1)
         self.fill_login_form()
-        time.sleep(2)
-        self.generate_cookies()
         self.select_captcha_answers()
         self.submit_info()
         time.sleep(2)
         self.slider_verify()
         time.sleep(3)
-        self.generate_cookies()
+        self.cookies = self.driver.get_cookies()
         print("登陆成功!")
 
     def save_cookies(self):
-        self.generate_cookies()
         CookieUtils.save_cookies(self.cookies)
         print("保存当前的cookies到文件cookies.json中！")
         self.driver.close()
         self.driver.quit()
-
-    def generate_cookies(self):
-        for cookie in self.driver.get_cookies():
-            if not any(ele['name'] == cookie['name'] for ele in self.cookies):
-                self.cookies.append(cookie)
 
     @staticmethod
     def generate_driver():
@@ -62,7 +53,6 @@ class BrowserAuth(Auth):
         actions.move_to_element(login_form)
         actions.click(login_form)
         actions.perform()
-        self.generate_cookies()
 
     def fill_login_form(self):
         username = self.driver.find_element_by_id("J-userName")
@@ -89,7 +79,6 @@ class BrowserAuth(Auth):
         submit_btn = self.driver.find_element_by_id("J-login")
         actions = ActionChains(self.driver)
         actions.move_to_element(submit_btn).click().perform()
-        self.generate_cookies()
 
     def slider_verify(self):
         slider = self.driver.find_element_by_id("nc_1_n1z")
